c26684e3fc0b2fc8cf64ee7cb2646f9eae0b7997
Abhilash Hegde <abhilashhegde00@gmail.com>
2022-06-12 18:46:48 +0530

    removed unused imports

--- src/atoms/TileLabel/index.js
+++ src/atoms/TileLabel/index.js
@@ -1,7 +1,5 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
-import { motion } from "framer-motion";
 
 const Title = styled.h4`
   margin: 5px 0px;
